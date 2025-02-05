175224f97420800153204fd1fda108dd6061d260
Darrel <darrelmschreiner@gmail.com>
2025-02-04 21:02:42 -0800

    added auth

--- src/components/list-header.tsx
+++ src/components/list-header.tsx
@@ -12,9 +12,14 @@ import { Link } from "expo-router";
 import { FontAwesome } from "@expo/vector-icons";
 import { CATEGORIES } from "assets/categories";
 import { useCartStore } from "@/store/cart-store";
+import { supabase } from "@/lib/supabase";
 
 export const ListHeader = () => {
   const { getItemCount } = useCartStore();
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+  };
+
   return (
     <View style={[styles.headerContainer]}>
       <View style={styles.headerTop}>
@@ -48,7 +53,10 @@ export const ListHeader = () => {
               )}
             </Pressable>
           </Link>
-          <TouchableOpacity style={styles.signOutButton}>
+          <TouchableOpacity
+            onPress={handleSignOut}
+            style={styles.signOutButton}
+          >
             <FontAwesome name="sign-out" size={25} color="red" />
           </TouchableOpacity>
         </View>
