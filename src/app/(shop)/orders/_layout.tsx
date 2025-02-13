4b7becf20a970fee46998563429265bdadcaba9a
Darrel <darrelmschreiner@gmail.com>
2025-02-13 09:38:40 -0800

    added subscriptions for real time order updates

--- src/app/(shop)/orders/_layout.tsx
+++ src/app/(shop)/orders/_layout.tsx
@@ -1,6 +1,9 @@
+import { useOrderUpdateSubscription } from "@/api/subscriptions";
 import { Stack } from "expo-router";
 
 export default function OrdersLayout() {
+  useOrderUpdateSubscription();
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{}} />
